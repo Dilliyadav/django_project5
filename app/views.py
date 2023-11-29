6f96ec108012731cea3305d3522a6a6761bd11c8
809949 <gunasrivinjam@gmail.com>
2023-11-29 14:28:42 +0530

    snd commit

--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def down(request):
     return render(request,'down.html')
+
+
+def btsrp(request):
+    return render(request,'btsrp.html')
--- project5/urls.py
+++ project5/urls.py
@@ -21,4 +21,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('down/',down,name='down'),
+    path('btsrp',btsrp,name='btsrp'),
 ]
